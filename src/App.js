d7de6ffb8c4a26249fe8e0d111d299ba008492ed
Tooba-Naz <naz.tooba34@gmail.com>
2023-11-02 22:38:02 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from "./Pages/Home/Index.js";
+import Products from "./Pages/Products/Index.js";
+import Cart from "./Components/Cart/Index.js";
+
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/products",
+    element: <Products/>
+  },
+  {
+    path: "/cart",
+    element: <Cart/>
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
+
 
 export default App;
